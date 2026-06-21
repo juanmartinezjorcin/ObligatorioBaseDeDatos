f16cb1409361449e354260be86149270d364a0bf
Daniel <daniel@192.168.1.61>
2026-06-20 23:05:16 -0300

    escaneo y validacion de QR implementadogit add .git add .

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
-    https: {},
     host: true,
+    allowedHosts: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
 })
